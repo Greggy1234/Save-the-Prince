e248017d568fe8143103d98959e9bbcffe5079ef
Greggy1234 <gregoreid@googlemail.com>
2025-08-21 16:48:59 +0100

    Set out basic javascript structure for the entire game

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,199 @@ let letter1 = "";
 let letter2 = "";
 let letter3 = "";
 
+// Button element event listeners
+// Once the DOM is loaded, all the buttons throughout the game will have an event listener and will be directed to the corresponding function
+
+
+
+
+// Global JavaScript code. The following apply throughout the whole game and are used for multiple sections
+/**
+ * The changing section function
+ * This is the first thing a player does. They input their name, and it gets stored to the global variable knightName
+ */
+
+
+/**
+ * The loading screen function
+ * This is the first thing a player does. They input their name, and it gets stored to the global variable knightName
+ */
+
+
+
+// Front page code
+/**
+ * The naming of the knight function
+ * This is the first thing a player does. They input their name, and it gets stored to the global variable knightName
+ */
+
+
+
+
+
+
+
+
+
+// Game hub area code
+/**
+ * The text area function specifically for the game hub area
+ * This will carry out all combinations of the text area copy in the correct order and based on the corect text tree. The number within the text cycle custom attribute will also change various elements visually
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Challenge one area code
+/**
+ * The text area function specifically for challenge one
+ * This will carry out all combinations of the text area copy in the correct order and based on the corect text tree. The number within the text cycle custom attribute will also change various elements visually
+ */
+
+
+
+/**
+ * Challenge one game function
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Challenge two area code
+/**
+ * The text area function specifically for challenge two
+ * This will carry out all combinations of the text area copy in the correct order and based on the corect text tree. The number within the text cycle custom attribute will also change various elements visually
+ */
+
+
+
+/**
+ * Challenge two game function
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Challenge three area code
+/**
+ * The text area function specifically for challenge three
+ * This will carry out all combinations of the text area copy in the correct order and based on the corect text tree. The number within the text cycle custom attribute will also change various elements visually
+ */
+
+
+
+/**
+ * Challenge three game function
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Challenge four area code
+/**
+ * The text area function specifically for challenge four
+ * This will carry out all combinations of the text area copy in the correct order and based on the corect text tree. The number within the text cycle custom attribute will also change various elements visually
+ */
+
+
+
+/**
+ * Challenge four game function
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Final area code

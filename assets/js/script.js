310e4a34e32547b5cadf595b10733f5e76eeaee5
Greggy1234 <gregoreid@googlemail.com>
2025-08-21 15:47:34 +0100

    Create basic hyml outline and input the global variables in the JS file

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Global Variables
+// These are for the various functions to access them throughout the game. THey get referenced mainly to set speech cycles and 
+let knightName = "";
+let letter1 = "";
+let letter2 = "";
+let letter3 = "";
+
